7f99ea94673fc0c956cdd761a8a8da779885dcb9
TheProgramSrc <git@theprogramsrc.xyz>
2020-08-12 12:53:29 -0400

    Changelog: * SimpleItem Code Improvements * Working on SkinTextureManager Cache Changes

--- src/main/java/xyz/theprogramsrc/supercoreapi/spigot/utils/skintexture/SkinTextureManager.java
+++ src/main/java/xyz/theprogramsrc/supercoreapi/spigot/utils/skintexture/SkinTextureManager.java
@@ -3,6 +3,7 @@ package xyz.theprogramsrc.supercoreapi.spigot.utils.skintexture;
 
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
+import xyz.theprogramsrc.supercoreapi.global.utils.Utils;
 import xyz.theprogramsrc.supercoreapi.spigot.items.Skulls;
 
 import java.util.HashMap;
@@ -23,23 +24,25 @@ public class SkinTextureManager {
      * @return the skin
      */
     public SkinTexture getSkin(Player player) {
-        if(!this.cache.containsKey(player.getName())){
+        String key = Utils.encodeBase64(player.getName());
+        if(!this.cache.containsKey(key)){
             SkinTexture skinTexture = Bukkit.getOnlineMode() ? SkinTexture.fromPlayer(player) : SkinTexture.fromMojang(player.getName());
             if(skinTexture == null){
                 return null;
             }
-            this.cache.put(player.getName(), skinTexture);
+            this.cache.put(key, skinTexture);
         }
-        return this.cache.get(player.getName());
+        return this.cache.get(key);
     }
 
     public SkinTexture getSkin(String url){
-        if(!this.urls.containsKey(url)){
+        String key = Utils.encodeBase64(url);
+        if(!this.urls.containsKey(key)){
             SkinTexture texture = SkinTexture.fromURL(url);
-            this.urls.put(url, texture);
+            this.urls.put(key, texture);
         }
 
-        return this.urls.get(url);
+        return this.urls.get(key);
     }
 
     /**
