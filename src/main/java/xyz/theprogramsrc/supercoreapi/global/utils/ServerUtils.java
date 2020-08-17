0819f399b761078df7d0b61c513d4dc42a6be067
TheProgramSrc <git@theprogramsrc.xyz>
2020-08-17 10:44:08 -0400

    Changelog: * Code Improvement

--- src/main/java/xyz/theprogramsrc/supercoreapi/global/utils/ServerUtils.java
+++ src/main/java/xyz/theprogramsrc/supercoreapi/global/utils/ServerUtils.java
@@ -14,11 +14,33 @@ public class ServerUtils {
     }
 
     /**
-     * Send a Player to another Server (Bukkit)
-     * @param player Player to send
-     * @param server Name of the Server to send
+     * Send a player to another server
+     * @param player the player
+     * @param server the server
      */
-    public void sendToServer(org.bukkit.entity.Player player, String server){
+    public void sendToServer(Object player, String server){
+        boolean canBungee = false;
+        try{
+            Class.forName("net.md_5.bungee.api.connection.ProxiedPlayer");
+            canBungee = true;
+        }catch (ClassNotFoundException ignored){ }
+
+        if(canBungee){
+            if(player instanceof net.md_5.bungee.api.connection.ProxiedPlayer){
+                this.sendToServerBungee(((net.md_5.bungee.api.connection.ProxiedPlayer)player), server);
+            }else{
+                this.sendToServerBukkit(((org.bukkit.entity.Player)player), server);
+            }
+        }else{
+            if(player instanceof org.bukkit.entity.Player){
+                this.sendToServerBukkit(((org.bukkit.entity.Player)player), server);
+            }else{
+                throw new IllegalArgumentException("The argument 'player' must be org.bukkit.entity.Player or net.md_5.bungee.api.connection.ProxiedPlayer");
+            }
+        }
+    }
+
+    private void sendToServerBukkit(org.bukkit.entity.Player player, String server){
         if(!((org.bukkit.plugin.java.JavaPlugin)this.plugin.getPlugin()).getServer().getMessenger().isOutgoingChannelRegistered(((org.bukkit.plugin.java.JavaPlugin)this.plugin.getPlugin()), "BungeeCord")){
             ((org.bukkit.plugin.java.JavaPlugin)this.plugin.getPlugin()).getServer().getMessenger().registerOutgoingPluginChannel(((org.bukkit.plugin.java.JavaPlugin)this.plugin.getPlugin()), "BungeeCord");
         }
@@ -35,12 +57,7 @@ public class ServerUtils {
         player.sendPluginMessage(((org.bukkit.plugin.java.JavaPlugin)this.plugin.getPlugin()), "BungeeCord", byteArrayOutputStream.toByteArray());
     }
 
-    /**
-     * Send a Player to another Server (Bungee)
-     * @param player Player to send
-     * @param server Name of the Server to send
-     */
-    public void sendToServer(net.md_5.bungee.api.connection.ProxiedPlayer player, String server){
+    private void sendToServerBungee(net.md_5.bungee.api.connection.ProxiedPlayer player, String server){
         ((net.md_5.bungee.api.plugin.Plugin)this.plugin).getProxy().registerChannel("BungeeCord");
         ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
         DataOutputStream dataOutputStream = new DataOutputStream(byteArrayOutputStream);
