24704efe4205ff9ee3b7864a9e12459d6a0cbfc9
Im-Fran <fran@theprogramsrc.xyz>
2020-10-19 21:44:02 -0300

    Removed Tests from SpigotPlugin.java

--- src/main/java/xyz/theprogramsrc/supercoreapi/plugin/SpigotPlugin.java
+++ src/main/java/xyz/theprogramsrc/supercoreapi/plugin/SpigotPlugin.java
@@ -1,57 +1,26 @@
 package xyz.theprogramsrc.supercoreapi.plugin;
 
-import org.bukkit.entity.Player;
-import xyz.theprogramsrc.supercoreapi.spigot.commands.CommandResult;
-import xyz.theprogramsrc.supercoreapi.spigot.commands.SpigotCommand;
-import xyz.theprogramsrc.supercoreapi.spigot.guis.action.ClickAction;
-import xyz.theprogramsrc.supercoreapi.spigot.guis.precreated.MaterialBrowser;
-import xyz.theprogramsrc.supercoreapi.spigot.utils.SpigotConsole;
-import xyz.theprogramsrc.supercoreapi.spigot.utils.xseries.XMaterial;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.plugin.java.JavaPlugin;
 
-public class SpigotPlugin extends xyz.theprogramsrc.supercoreapi.spigot.SpigotPlugin {
-
+public class SpigotPlugin extends JavaPlugin {
 
     @Override
-    public void onPluginLoad() {
-
+    public void onEnable() {
+        this.log("&3SuperCoreAPI &awas loaded as .jar file");
     }
 
     @Override
-    public void onPluginEnable() {
-        new Command();
+    public void onDisable() {
+        this.log("&3SuperCoreAPI &awas disabled");
     }
 
-    @Override
-    public void onPluginDisable() {
-
+    private void log(String s){
+        Bukkit.getConsoleSender().sendMessage(c(s));
     }
 
-    static class Command extends SpigotCommand {
-
-        @Override
-        public String getCommand() {
-            return "scapi";
-        }
-
-        @Override
-        public CommandResult onPlayerExecute(Player player, String[] args) {
-            new MaterialBrowser(player){
-                @Override
-                public void onBack(ClickAction clickAction) {
-                    this.close();
-                }
-
-                @Override
-                public void onSelect(ClickAction clickAction, XMaterial xMaterial) {
-                    clickAction.getPlayer().getInventory().addItem(xMaterial.parseItem());
-                }
-            }.open();
-            return CommandResult.COMPLETED;
-        }
-
-        @Override
-        public CommandResult onConsoleExecute(SpigotConsole console, String[] args) {
-            return CommandResult.COMPLETED;
-        }
+    private String c(String s){
+        return ChatColor.translateAlternateColorCodes('&', s);
     }
 }
