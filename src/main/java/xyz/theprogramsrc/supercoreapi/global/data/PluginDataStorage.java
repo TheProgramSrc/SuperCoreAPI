0efc90bd17a060fc1168a920418011b362c4ee0b
Francisco Solis <fran@theprogramsrc.xyz>
2021-06-25 17:32:30 -0400

    Fix for Spigot 1.17 * Fixed SuperCoreAPICommand Info not working in Spigot 1.17+ * Added new options for PluginDataStorage

--- src/main/java/xyz/theprogramsrc/supercoreapi/global/data/PluginDataStorage.java
+++ src/main/java/xyz/theprogramsrc/supercoreapi/global/data/PluginDataStorage.java
@@ -10,10 +10,20 @@ public class PluginDataStorage extends JsonConfig {
     public PluginDataStorage(SuperPlugin<?> plugin){
         super(plugin.getPluginFolder(), "PluginData.json");
         this.add("stats_id", UUID.randomUUID().toString());
+        this.add("low_resource_usage", false);
+        this.add("share_stats", true);
         this.add("debug", "false");
     }
 
     public boolean isDebugEnabled(){
         return this.getBoolean("debug");
+    }
+
+    public boolean isLowResourceUsageEnabled(){
+        return this.getBoolean("low_resource_usage");
+    }
+
+    public boolean isShareStatsEnabled(){
+        return this.getBoolean("share_stats");
     }
 }
